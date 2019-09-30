0526e29bc5740157711e20379b16055d4de6cb4c
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-30 08:59:23 -0300

    Projeto criado

---
+++ src/test/java/com/gabrielcamp/workshopmongo/WorkshopSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gabrielcamp.workshopmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
