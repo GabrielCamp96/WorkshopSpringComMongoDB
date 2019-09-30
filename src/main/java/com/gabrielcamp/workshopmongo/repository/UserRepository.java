84db11f2d94a229e87178f6332388554e00f2071
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-30 10:15:32 -0300

    Entidade User criada e REST funcionando

---
+++ src/main/java/com/gabrielcamp/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.gabrielcamp.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.config.MongoRepositoryConfigurationExtension;
+
+public class UserRepository extends MongoRepositoryConfigurationExtension {
+
+}
---
+++ src/main/java/com/gabrielcamp/workshopmongo/services/UserService.java
@@ -0,0 +1,12 @@
+package com.gabrielcamp.workshopmongo.services;
+
+import org.springframework.stereotype.Service;
+
+import com.gabrielcamp.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+//	UserRepository repo;
+	
+}
