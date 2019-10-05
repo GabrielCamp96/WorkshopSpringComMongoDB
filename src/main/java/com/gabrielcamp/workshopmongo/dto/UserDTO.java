145cc451ff30195092867dc8dfe7ea0bc5d7273a
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-10-04 21:23:13 -0300

    Criacao UserDTO e UserResource Refatorado

---
+++ src/main/java/com/gabrielcamp/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package com.gabrielcamp.workshopmongo.dto;
+
+import com.gabrielcamp.workshopmongo.domain.User;
+
+public class UserDTO {
+
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		
+	}
+
+	public UserDTO(User user) {
+		super();
+		this.id = user.getId();
+		this.name = user.getName();
+		this.email = user.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
